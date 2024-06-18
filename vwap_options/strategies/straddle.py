c5c9b21e3f5ce83761186549cc0d32b94b44659a
pannet1 <prog@ecomsense.in>
2024-06-18 20:14:22 +0530

    refactor

--- vwap_options/strategies/straddle.py
+++ vwap_options/strategies/straddle.py
@@ -16,15 +16,9 @@ def extract_strike(symbol):
 
 class StraddleStrategy:
     def __init__(self, api, base, base_info, ul):
-        self._strategy = {
-            "old": 0,
-            "pnl": 0,
-            "is_started": False,
-            "is_ce_position": False,
-            "is_pe_position": False,
-            "ce_symbol": "",
-            "pe_symbol": "",
-        }
+        self._strategy = {"is_started": False}
+        self._ce = {"is_position": False}
+        self._pe = {"is_position": True}
         self._api = api
         self._timer = pdlm.now()
         self._ul = ul
@@ -63,11 +57,13 @@ class StraddleStrategy:
                     args["order_id"] = resp
                     flag = True
                     if option_type == "ce":
-                        self._strategy["is_ce_position"] = flag
-                        self._strategy["ce_stop"] = args
+                        self._ce["ce"] = symbol
+                        self._ce["is_position"] = flag
+                        self._ce["stop"] = args
                     else:
-                        self._strategy["is_pe_position"] = flag
-                        self._strategy["pe_stop"] = args
+                        self._pe["pe"] = symbol
+                        self._pe["is_position"] = flag
+                        self._pe["stop"] = args
                     return flag
         except Exception as e:
             logging.error(f"Error enter positions: {e}")
@@ -77,7 +73,9 @@ class StraddleStrategy:
 
     def exit_position(self, option_type):
         print(f"closing {option_type}")
-        resp_stop = self._strategy[f"{option_type}_stop"]
+        attribute = f"_{option_type}"
+        option = getattr(self, attribute)
+        resp_stop = option["stop"]
         args = dict(
             order_id=resp_stop["order_id"],
             tradingsymbol=resp_stop["symbol"],
@@ -87,7 +85,10 @@ class StraddleStrategy:
             self._api.order_modify(**args)
         else:
             self._api.order_modify(**resp_stop)
-        self._strategy[f"is_{option_type}_position"] = False
+        option["is_position"] = False
+        # delete key/value in option["stop"]
+        del option["stop"]
+        setattr(self, attribute, option)
 
     def exit_positions(self):
         for pos in self._api.positions:
@@ -106,7 +107,6 @@ class StraddleStrategy:
                 resp = self._api.order_place(**args)
                 logging.debug(args)
                 logging.debug(resp)
-        self._strategy["entry"] = 0
 
     def get_spot_and_mkt_atm(self):
         lp = ApiHelper().scriptinfo(self._api, self._ul["exchange"], self._ul["token"])
@@ -114,7 +114,7 @@ class StraddleStrategy:
 
     def option_info(self, c_or_p):
         option_type = "C" if c_or_p == "ce" else "P"
-        spot, atm = self.get_spot_and_mkt_atm()
+        _, atm = self.get_spot_and_mkt_atm()
         return self._symbol.find_option_by_distance(
             atm,
             self._base_info["away_from_atm"],
@@ -128,8 +128,8 @@ class StraddleStrategy:
             self._tokens = self._symbol.get_tokens(atm)
             self._strategy["spot"] = spot
             self._strategy["atm"] = atm
-            self._strategy["upper_band"] = spot + self._base_info["band_width"]
-            self._strategy["lower_band"] = spot - self._base_info["band_width"]
+            self._ce["band"] = spot + self._base_info["band_width"]
+            self._pe["band"] = spot - self._base_info["band_width"]
             self._strategy["is_started"] = True
             self.enter_position("ce")
             self.enter_position("pe")
@@ -138,47 +138,52 @@ class StraddleStrategy:
             traceback.print_exc()
 
     def update_bands(self, current_spot):
-        upper_band = self._strategy["upper_band"]
-        lower_band = self._strategy["lower_band"]
+        upper_band = self._ce["band"]
+        lower_band = self._pe["band"]
         band_width = self._base_info["band_width"]
 
         upper_band_limit = upper_band + band_width * 3
         lower_band_limit = lower_band - band_width * 3
 
         if current_spot > upper_band_limit:
-            self._strategy["upper_band"] = upper_band + band_width
+            self._ce["band"] = upper_band + band_width
 
         if current_spot < lower_band_limit:
-            self._strategy["lower_band"] = lower_band - band_width
+            self._pe["band"] = lower_band - band_width
 
     def on_tick(self):
         try:
-            current_spot, atm = self.get_spot_and_mkt_atm()
+            current_spot, _ = self.get_spot_and_mkt_atm()
             self._strategy["spot"] = current_spot
             self.update_bands(current_spot)
             self._timer = self._timer.add(seconds=60)
-            self.check_and_update_position("ce", "upper_band", current_spot)
-            self.check_and_update_position("pe", "lower_band", current_spot)
+            self.check_and_update_position("ce")
+            self.check_and_update_position("pe")
             self._display.at(2, self._strategy)
+            self._display.at(3, self._ce)
+            self._display.at(4, self._pe)
         except Exception as e:
             logging.error(f"on tick error as {e}")
             traceback.print_exc()
 
-    def check_and_update_position(self, position, band, spot):
-        position_key = f"is_{position}_position"
-        if self._strategy[position_key]:
-            if self.check_spot(spot, band):
+    def check_and_update_position(self, position):
+        spot = self._strategy["spot"]
+        option = getattr(self, f"_{position}")
+        if option["is_position"]:
+            if self.check_spot(spot, position):
                 self.exit_position(position)
-                self._strategy[position_key] = False
-        if not self._strategy[position_key]:
-            if not self.check_spot(spot, band):
+                option["is_position"] = False
+                setattr(self, f"_{position}", option)
+        if not option["is_position"]:
+            if not self.check_spot(spot, position):
                 self.enter_position(position)
 
-    def check_spot(self, spot, band):
-        print(f"current_spot: {spot} {band}: {self._strategy[band]}")
-        if band == "upper_band":
-            return spot > self._strategy[band]
-        return spot < self._strategy[band]
+    def check_spot(self, spot, position):
+        option = getattr(self, f"_{position}")
+        print(f"current_spot: {spot} {position} band: {option['band']}")
+        if position == "ce":
+            return spot > self._ce["band"]
+        return spot < self._pe["band"]
 
     def run(self):
         while not is_time_past(STOP):
@@ -191,13 +196,13 @@ class StraddleStrategy:
                 self.on_tick()
             else:
                 UTIL.slp_for(1)
-            self._display.at(3, self._api.positions)
+            self._display.at(5, self._api.positions)
         else:
             if self._strategy["is_started"]:
                 self.exit_positions()
                 self._strategy["is_started"] = False
             if not CMMN["live"]:
-                self._display.at(3, self._api.positions)
+                self._display.at(5, self._api.positions)
                 logging.debug("converting orders to positions in paper mode")
                 df = pd.read_csv(DATA + "orders.csv")
                 if not df.empty:
